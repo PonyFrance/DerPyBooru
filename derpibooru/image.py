929c018b823e6998f380d67a3e9b53264b9f4279
Gian Sass <gian.sass@outlook.de>
2016-07-04 11:32:54 -0400

    Change id_number to id

--- derpibooru/image.py
+++ derpibooru/image.py
@@ -51,7 +51,7 @@ class Image(object):
 
 
   def __str__(self):
-    return "Image({0})".format(self.id_number)
+    return "Image({0})".format(self.id)
 
   @property
   def tags(self):
@@ -126,14 +126,14 @@ class Image(object):
        
   @property
   def url(self):
-    return "https://derpibooru.org/{}".format(self.id_number)
+    return "https://derpibooru.org/{}".format(self.id)
 
   @property
   def data(self):
     return self._data
 
   def update(self):
-    data = get_image_data(self.id_number)
+    data = get_image_data(self.id)
 
     if data:
       self._data = data
